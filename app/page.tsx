7c6d0dae7ed1e92816391c5fb2ee99f8fe2bd029
choidy180 <choidy180@naver.com>
2025-12-08 16:52:03 +0900

    'deploy'

--- app/page.tsx
+++ app/page.tsx
@@ -64,7 +64,7 @@ export default function ExaoneChatInterface() {
         </HistoryList>
 
         <UserProfileArea>
-          <UserCard>
+          <UserCard onClick={()=> router.push('/admin/database')}>
             <Avatar>U</Avatar>
             <UserName>User Settings</UserName>
           </UserCard>
